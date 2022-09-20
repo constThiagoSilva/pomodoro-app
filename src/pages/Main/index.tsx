28eda6cd1f74b882a4d9b1785e420cd8a3427319
drakeira <thiago.p.dasilva2005@gmail.com>
2022-09-19 21:49:01 -0300

    feat: add timer

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,12 +1,35 @@
+import { useEffect, useState } from "react";
 import { PlayButton } from "../../components/PlayButton"
 import { Container, Content, Timer } from "./styles"
 
 export const MainPage = () => {
+    const [minutes, setMinutes] = useState(10);
+    const [minutesLeft, setMinutesLeft] = useState(2);
+    const [secondsLeft, setSecondsLeft] = useState(0);
+
+    useEffect(() => {
+        let interval = setInterval(() => {
+            clearInterval(interval)
+
+            if (secondsLeft === 0) {
+                if (minutesLeft !== 0) {
+                    setSecondsLeft(59)
+                    setMinutesLeft(minutesLeft - 1)
+                }
+            } else {
+                setSecondsLeft(secondsLeft - 1)
+            }
+
+        }, 1000)
+    }, [secondsLeft]);
+
     return (
         <Container>
             <Content>
                 <Timer>
-                    <span>00:00</span>
+                    <span>
+                        {minutesLeft < 9 ? `0${minutesLeft}` : minutesLeft}:{secondsLeft < 9 ? `0${secondsLeft}` : secondsLeft}
+                    </span>
                 </Timer>
                 <PlayButton/>
             </Content>
